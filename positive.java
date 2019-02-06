7929e7a6da105a8bd753a18a0cfeb6d2c3b17ce1
saranyaesris <47380941+saranyaesris@users.noreply.github.com>
2019-02-06 14:14:56 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,49 @@
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+public class myprogram
+
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	    
+int n=3;
+	   
+if(n==3)
+	   
+{
+	        
+System.out.println("Positive");
+	    
+}
+	    
+else
+	    
+if(n==-3)
+	    
+{
+	        
+System.out.println("Negative");
+	    
+}
+	    
+else
+	    
+if(n==0)
+	    
+{
+	        
+System.out.println("Zero");
+	        
+}
+	    
+	    
+}
+	
+}
