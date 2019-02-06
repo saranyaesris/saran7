456cceb5f79f2ce4c40d3ba09867f8f29c1e893b
saranyaesris <47380941+saranyaesris@users.noreply.github.com>
2019-02-06 14:20:30 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+public class myprogram
+
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+
+int 
+n=3;
+
+if(n==3)
+
+{
+    
+System.out.println("Odd");
+
+}
+
+else
+
+{
+    
+System.out.println("Even");
+	
+}
+	
+}
+
+}
